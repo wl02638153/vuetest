d3befa93e2322e87162d75fce98f513ae6c24df9
JackWei <wl02638153@gmail,com>
2020-08-01 12:30:15 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -24,5 +24,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+>>> p{
+  color:red;
 }
 </style>
